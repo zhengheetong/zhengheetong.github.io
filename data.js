2604f2592ce454c9e620122e218b0f10f2941b22
zhengheetong <zhenghee535@gmail.com>
2026-03-05 20:54:11 +0800

    added cert view

---
+++ data.js
@@ -0,0 +1,19 @@
+const myBootCert = [
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/87254722-5a84-473e-812d-d20f5f7f33ab.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/9028fea2-470c-442a-88a6-cca997ad0d69.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/b181a7a8-1772-4ea0-b4f1-5caee5ba262d.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/23afa8fb-4701-4b78-ab4f-a864d59fb7f4.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/dafad8ed-3ab6-4de1-9468-7bcac93a3713.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/6291fabc-d06d-4e03-92dd-4fb26158841b.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/45399803-8bdd-4b56-8817-7761a7870c2d.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/3731109d-c67c-4ac6-8f37-41d5332a4f52.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/39d134dd-74ad-4513-9f24-e5696901a6e1.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/3f2eba4c-527b-4ed1-8a68-f3227b2d9fe2.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/d3f6b5f1-eca0-4974-b3f0-8c9f74c5e8d4.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/8c84cd98-ae15-4e16-b478-0b25af1c9ea7.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/faf85477-fe2b-4bf3-8d71-28aa506ced55.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/74854a87-0878-428b-8bbb-413eb7679cf6.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/abec6428-98e5-46d6-ba8f-429e9b07d9d9.jpeg",
+    "https://qvault-webapp-dynamic-assets.storage.googleapis.com/certificates/f7759cfb-feaf-4688-9730-792b57c35989.jpeg",
+];
+
